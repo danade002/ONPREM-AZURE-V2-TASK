909ada7d5f785d5b6641838f4592ef2463f7e180
danade002 <danielademeso1@gmail.com>
2024-06-05 19:06:35 +0100

    onprem azure v2

--- main.tf
+++ main.tf
@@ -12,14 +12,14 @@ provider "azurerm" {
 }
 
 module "resource_group" {
-  source = "./modules/resource_group"
+  source              = "./modules/resource_group"
   resource_group_name = var.resource_group_name
   location            = var.location
 }
 
 
 module "virtual_network" {
-  source = "./modules/virtual_network"
+  source              = "./modules/virtual_network"
   resource_group_name = module.resource_group.resource_group_name
   location            = var.location
   vnet_name           = var.vnet_name
@@ -27,34 +27,34 @@ module "virtual_network" {
 }
 
 module "subnet" {
- source = "./modules/subnet"
+  source              = "./modules/subnet"
   resource_group_name = module.resource_group.resource_group_name
-  vnet_name = module.virtual_network.vnet_name
+  vnet_name           = module.virtual_network.vnet_name
   subnet_name         = var.subnet_name
   address_prefix      = var.subnet_address_prefix
 }
 
 module "linux_virtual_machine" {
-  source = "./modules/linux_virtual_machine"
-  resource_group_name = module.resource_group.resource_group_name
-  location            = var.location
-  vm_name             = var.vm_name
-  vm_size             = var.vm_size
-  subnet_id           = module.subnet.subnet_id
-  admin_username      = var.admin_username
-  admin_ssh_key       = var.admin_ssh_key
+  source               = "./modules/linux_virtual_machine"
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = var.location
+  vm_name              = var.vm_name
+  vm_size              = var.vm_size
+  subnet_id            = module.subnet.subnet_id
+  admin_username       = var.admin_username
+  admin_ssh_key        = var.admin_ssh_key
   network_interface_id = var.network_interface_id
 }
 
 module "container_registry" {
-  source = "./modules/container_registry"
+  source              = "./modules/container_registry"
   resource_group_name = module.resource_group.resource_group_name
   location            = var.location
   registry_name       = var.registry_name
 }
 
 module "postgresql" {
-  source = "./modules/postgresql"
+  source              = "./modules/postgresql"
   resource_group_name = module.resource_group.resource_group_name
   location            = var.location
   server_name         = var.pg_server_name
@@ -62,14 +62,14 @@ module "postgresql" {
 }
 
 module "blob_storage" {
-  source = "./modules/blob_storage"
-  resource_group_name = module.resource_group.resource_group_name
-  location            = var.location
+  source               = "./modules/blob_storage"
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = var.location
   storage_account_name = var.storage_account_name
 }
 
 module "dns" {
-  source = "./modules/dns"
+  source              = "./modules/dns"
   resource_group_name = module.resource_group.resource_group_name
   location            = var.location
   dns_zone_name       = var.dns_zone_name
