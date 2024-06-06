440aed92c68150e15803c348783dc4ec70872bc5
danade002 <danielademeso1@gmail.com>
2024-06-06 16:08:09 +0100

    Fixing the apply issue

--- modules/linux_virtual_machine/main.tf
+++ modules/linux_virtual_machine/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("../../admin_ssh_key.pub")
+    public_key = file("./admin_ssh_key.pub")
   }
 }
 
