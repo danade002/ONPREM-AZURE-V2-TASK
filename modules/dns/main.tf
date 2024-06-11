403b9119aebbc78d16ada3fb5146804fcfc7900c
danade002 <danielademeso1@gmail.com>
2024-06-11 14:37:37 +0100

    Manage SSL and Subdomains in Az

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_dns_zone" "azurerm_dns_zone.dns_zone " {
+resource "azurerm_dns_zone" "main" {
   name                = var.domain_name
   resource_group_name = var.resource_group_name
 }
