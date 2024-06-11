5384a95a51bcf05a1fb563187e801a62de5e3bba
danade002 <danielademeso1@gmail.com>
2024-06-11 13:35:42 +0100

    Manage SSL and Subdomains in Az

--- modules/linux_virtual_machine/main.tf
+++ modules/linux_virtual_machine/main.tf
@@ -42,10 +42,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   size                  = var.vm_size
   admin_username        = var.admin_username
 
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.admin_ssh_key
-  }
+ 
 
   os_disk {
     caching              = "ReadWrite"
