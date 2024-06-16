c9df5a960b0d314bc205b8d5f14fd0fd12bc17f3
danade002 <danielademeso1@gmail.com>
2024-06-16 21:31:28 +0100

    chore: Update Terraform backend configuration

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
     }
   }
   backend "azurerm" {
-    container_name = "tfstate"
-    key            = "terraform.tfstate"
+    container_name = "terraform.tfstate"
+    key            = "tfstate"
   }
 }
 
