0b6918d99e4d16d0c3de849d0f1f243a43f64d28
danade002 <danielademeso1@gmail.com>
2024-06-19 16:39:03 +0100

    configure resource groups & key vaults

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,9 +1,15 @@
-data "azurerm_resource_group" "existing" {
+data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
-resource "azurerm_resource_group" "example" {
-  count    = length(data.azurerm_resource_group.existing.id) == 0 ? 1 : 0
+resource "azurerm_resource_group" "rg-new" {
+  count    = length(data.azurerm_resource_group.rg.id) == 0 ? 1 : 0
   name     = var.resource_group_name
   location = var.location
 }
+
+output "resource_group_id" {
+  value = length(data.azurerm_resource_group.rg.id) == 0 ? azurerm_resource_group.rg-new[0].id : data.azurerm_resource_group.rg.id
+}
+
+
