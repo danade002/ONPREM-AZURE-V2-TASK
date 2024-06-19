d66be7d80263e466750d9aece2d8f96bddfeb553
danade002 <danielademeso1@gmail.com>
2024-06-19 15:25:15 +0100

    configure resource groups & key vaults

--- modules/key_vault/outputs.tf
+++ modules/key_vault/outputs.tf
@@ -1,8 +1,19 @@
+output "key_vault_name" {
+  description = "The name of the Key Vault"
+  value       = azurerm_key_vault.key_vault.name
+}
+
+output "key_vault_uri" {
+  description = "The URI of the Key Vault"
+  value       = azurerm_key_vault.key_vault.vault_uri
+}
 
 output "admin_login_secret_id" {
-  value = azurerm_key_vault_secret.admin_login_secret.id
+  description = "The ID of the administrator login secret"
+  value       = azurerm_key_vault_secret.admin_login.id
 }
 
 output "admin_password_secret_id" {
-  value = azurerm_key_vault_secret.admin_password_secret.id
+  description = "The ID of the administrator login password secret"
+  value       = azurerm_key_vault_secret.admin_password.id
 }
