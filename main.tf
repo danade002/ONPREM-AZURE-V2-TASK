a6e819f6ff80fa57bf4cc8e9dd84eb18fc623455
danade002 <danielademeso1@gmail.com>
2024-06-19 11:06:53 +0100

    configure resource groups

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ resource_group_name = var.resource_group_name
 
 
 module "azurerm_resource_group" {
-  source = "./modules/azurerm_resource_group"
+  source = "./modules/resource_group"
   resource_group_name = var.resource_group_name
   location = var.location
   
