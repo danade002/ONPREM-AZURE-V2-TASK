0f97adab4bfa18049d2bdc3a4ad67fb32d05dede
danade002 <danielademeso1@gmail.com>
2024-06-20 17:20:43 +0100

    resource group

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -1,7 +1,7 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = var.resource_group_name
 }
 
 output "location" {
-  value = azurerm_resource_group.rg.location
+  value = var.location
 }
