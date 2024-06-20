35139fe0e00b64cba34f0f8dacf8790075dd668f
danade002 <danielademeso1@gmail.com>
2024-06-20 11:26:33 +0100

    Azure key vaults

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -4,10 +4,11 @@ resource "azurerm_key_vault" "key_vault" {
   name                        = var.key_vault_name
   location                    = var.location
   resource_group_name         = var.resource_group_name
-  tenant_id         = data.azurerm_client_config.current.tenant_id
   enabled_for_disk_encryption = var.enabled_for_disk_encryption
+  tenant_id = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = var.soft_delete_retention_days
   purge_protection_enabled    = var.purge_protection_enabled
+  
   sku_name = var.sku_name
 
 }
@@ -23,6 +24,5 @@ resource "azurerm_key_vault_secret" "admin_password" {
   value        = var.administrator_login_password
   key_vault_id = azurerm_key_vault.key_vault.id
 }
-
 
 data "azurerm_client_config" "current" {}
