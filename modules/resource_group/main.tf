83ddaede06e695dcd331d89c468e09ddf267d3ef
danade002 <danielademeso1@gmail.com>
2024-06-21 06:13:18 +0100

    resource group

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,16 +1,14 @@
 resource "azurerm_resource_group" "new_rg" {
-  count    = var.resource_group_name ? 0 : 1
+  count    = var.use_existing ? 0 : 1
   name     = var.new_name
   location = var.location
 }
 
-data "azurerm_resource_group" "resource_group_name" {
+data "azurerm_resource_group" "existing_rg" {
   count = var.use_existing ? 1 : 0
   name  = var.existing_name
 }
 
 locals {
-  resource_group_name = var.use_existing ? data.azurerm_resource_group.resource_group_name[0].name : azurerm_resource_group.new_rg[0].name
+  resource_group_name = var.use_existing ? data.azurerm_resource_group.existing_rg[0].name : azurerm_resource_group.new_rg[0].name
 }
-
-
