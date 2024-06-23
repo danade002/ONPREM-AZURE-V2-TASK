08fb1655e2be0f2a2c55b90be6a8b7eecd01d6f5
danade002 <danielademeso1@gmail.com>
2024-06-23 15:05:59 +0100

    res

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -5,7 +5,3 @@ output "resource_group_name" {
   value = var.resource_group_name
 }
 
-output "existing_rg_id" {
-  description = "The ID of the existing Azure Resource Group."
-  value       = try(azurerm_resource_group.existing_rg[0].id, null)
-}
