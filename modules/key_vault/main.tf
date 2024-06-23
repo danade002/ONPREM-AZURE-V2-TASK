d69358cb80368f6f8824eb3fdba339a04183bfca
danade002 <danielademeso1@gmail.com>
2024-06-23 22:01:48 +0100

    Az key vaults

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -2,11 +2,25 @@ resource "azurerm_key_vault" "key_vault" {
   name                        = var.key_vault_name
   location                    = var.location
   resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
   sku_name                    = var.sku_name
   soft_delete_retention_days  = var.soft_delete_retention_days
   purge_protection_enabled    = var.purge_protection_enabled
+  tenant_id                  = data.azurerm_client_config.example.tenant_id
 }
+ data "azurerm_client_config" "example" {}
+
+resource "azurerm_key_vault_access_policy" "azurerm_key_vault_access_policy" {
+  key_vault_id = azurerm_key_vault.azurerm_key_vault_access_policy.id
+  tenant_id    = data.azurerm_client_config.example.tenant_id
+  object_id    = data.azurerm_client_config.example.object_id
+
+  secret_permissions = [
+    "Get",
+    "Set",
+    "List",
+  ]
+}
+
 
 resource "azurerm_key_vault_secret" "admin_login" {
   name         = "administrator-login"
@@ -19,3 +33,5 @@ resource "azurerm_key_vault_secret" "admin_password" {
   value        = var.administrator_login_password
   key_vault_id = azurerm_key_vault.key_vault.id
 }
+
+
