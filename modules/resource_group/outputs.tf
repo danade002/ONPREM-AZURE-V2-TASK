494f1ba63e4569c116b365f8947bea38dc01813b
danade002 <danielademeso1@gmail.com>
2024-06-23 14:59:38 +0100

    res

--- modules/resource_group/outputs.tf
+++ modules/resource_group/outputs.tf
@@ -4,3 +4,13 @@ output "location" {
 output "resource_group_name" {
   value = var.resource_group_name
 }
+
+output "existing_rg_id" {
+  description = "The ID of the existing resource group"
+  value       = azurerm_resource_group.existing_rg.id
+}
+
+
+output "existing_rg_id_from_module" {
+  value = module.resource_group_module.existing_rg_id
+}
--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "existing_rg_id" {
-  description = "The ID of the existing resource group"
-  value       = azurerm_resource_group.existing_rg.id
-}
-
-
-output "existing_rg_id_from_module" {
-  value = module.resource_group_module.existing_rg_id
-}
