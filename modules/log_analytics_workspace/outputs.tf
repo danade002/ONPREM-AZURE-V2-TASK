93e03138d197a443ffd42585a237d325abf25355
danade002 <danielademeso1@gmail.com>
2024-06-24 23:00:12 +0100

    Monitoring & alerts set up

--- modules/log_analytics_workspace/outputs.tf
+++ modules/log_analytics_workspace/outputs.tf
@@ -1,14 +1,5 @@
 output "id" {
   description = "The ID of the Log Analytics Workspace."
-  value       = azurerm_log_analytics_workspace.this.id
+  value       = azurerm_log_analytics_workspace.log_analytics_workspace.id
 }
 
-output "workspace_id" {
-  description = "The Workspace ID of the Log Analytics Workspace."
-  value       = azurerm_log_analytics_workspace.this.workspace_id
-}
-
-output "primary_shared_key" {
-  description = "The Primary Shared Key of the Log Analytics Workspace."
-  value       = azurerm_log_analytics_workspace.this.primary_shared_key
-}
