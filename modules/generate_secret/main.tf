2abdd8a761fc684bc27ea8b5c1c89d5dcb9ce20b
danade002 <danielademeso1@gmail.com>
2024-06-26 12:00:12 +0100

    conditions for  az key vaults value

--- modules/generate_secret/main.tf
+++ modules/generate_secret/main.tf
@@ -1,13 +1,12 @@
-resource "random_password" "admin_password" {
-  count           = var.use_generate_secret ? 1 : 0
-  length          = var.generate_secret_length
-  special         = var.generate_secret_special
-  override_special = "_%@"
+resource "random_password" "administrator-login-password" {
+  count  = var.use_generate_secret ? 1 : 0
+  length = var.generate_secret_length
+  special = var.generate_secret_special
 }
 
 resource "azurerm_key_vault_secret" "administrator-login-password_secret" {
   count       = var.use_generate_secret ? 1 : 0
   name        = "administrator-login-password"
-  value       = random_password.administrator-login-password[count.index].result
+  value       = random_password.administrator-login-password[0].result
   key_vault_id = var.key_vault_id
 }
--- modules/generate_secret/outputs.tf
+++ modules/generate_secret/outputs.tf
@@ -2,8 +2,3 @@ output "generated_password" {
   value       = length(random_password.administrator-login-password) > 0 ? random_password.administrator-login-password[0].result : ""
   description = "The generated admin password"
 }
-
-output "admin_password_secret_id" {
-  value       = length(azurerm_key_vault_secret.administrator-login-password_secret) > 0 ? azurerm_key_vault_secret.administrator-login-password_secret[0].id : ""
-  description = "ID of the Key Vault secret for generated admin password"
-}
