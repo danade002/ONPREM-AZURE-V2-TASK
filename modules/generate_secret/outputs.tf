ed2d9c0a31a0729bcd0b21493c01c52e53f093a0
danade002 <danielademeso1@gmail.com>
2024-06-27 05:28:18 +0100

    conditions for  az key vaults value for use_generate_secret

--- modules/generate_secret/outputs.tf
+++ modules/generate_secret/outputs.tf
@@ -1,9 +1,9 @@
-output "generated_password" {
-  value       = length(random_password.admin_password) > 0 ? random_password.admin_password[0].result : ""
-  description = "The generated admin password"
+output "generated_secret_id" {
+  description = "ID of the generated secret in Azure Key Vault"
+  value       = azurerm_key_vault_secret.generated_secret.id
 }
 
-output "admin_password_secret_id" {
-  value       = length(azurerm_key_vault_secret.admin_password_secret) > 0 ? azurerm_key_vault_secret.admin_password_secret[0].id : ""
-  description = "ID of the Key Vault secret for admin password"
+output "generated_secret_name" {
+  description = "Name of the generated secret in Azure Key Vault"
+  value       = azurerm_key_vault_secret.generated_secret.name
 }
