1cd3ee63eff57dccba27fe3ccbcba1d980fbd95f
danade002 <danielademeso1@gmail.com>
2024-06-27 05:33:10 +0100

    conditions for  az key vaults value for use_generate_secret

--- modules/generate_secret/outputs.tf
+++ modules/generate_secret/outputs.tf
@@ -1,9 +1,9 @@
 output "generated_secret_id" {
   description = "ID of the generated secret in Azure Key Vault"
-  value       = azurerm_key_vault_secret.generated_secret.id
+  value       = azurerm_key_vault_secret.generated_secret[*].id  # Access all IDs using splat expression
 }
 
 output "generated_secret_name" {
   description = "Name of the generated secret in Azure Key Vault"
-  value       = azurerm_key_vault_secret.generated_secret.name
+  value       = azurerm_key_vault_secret.generated_secret[*].name  # Access all names using splat expression
 }
