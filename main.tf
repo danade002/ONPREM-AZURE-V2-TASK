341482f1eb4e8d03de75034a9a613643ec482db6
danade002 <danielademeso1@gmail.com>
2024-06-30 06:30:00 +0100

    conditions for  az key vaults value for use admin credentials

--- main.tf
+++ main.tf
@@ -97,7 +97,6 @@ module "key_vault" {
   source              = "./modules/key_vault"
   location            = var.location
   resource_group_name = var.resource_group_name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = var.sku_name
   soft_delete_retention_days = var.soft_delete_retention_days
   purge_protection_enabled = var.purge_protection_enabled
