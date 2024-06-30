370893573115b44378dc18e4beb46ee5e0bf9aa0
danade002 <danielademeso1@gmail.com>
2024-06-30 22:56:29 +0100

    use admincredentials

--- modules/key_vault_secrets/outputs.tf
+++ modules/key_vault_secrets/outputs.tf
@@ -1,14 +1,7 @@
 output "admin_login_secret_id" {
-  description = "The ID of the administrator login secret in Azure Key Vault."
-  value       = azurerm_key_vault_secret.admin_login.id
+  value = azurerm_key_vault_secret.admin_login[*].id
 }
 
 output "admin_login_password_secret_id" {
-  description = "The ID of the administrator login password secret in Azure Key Vault."
-  value       = azurerm_key_vault_secret.admin_login_password.id
-}
-
-output "generated_secrets" {
-  description = "Generated secrets stored in Azure Key Vault."
-  value       = azurerm_key_vault_secret.secrets
+  value = azurerm_key_vault_secret.admin_login_password[*].id
 }
