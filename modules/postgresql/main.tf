0eb73d8dd5591f119db883299c2b90953bf774c9
danade002 <danielademeso1@gmail.com>
2024-07-01 19:58:40 +0100

    fixing comments on azure key vaults

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -1,10 +1,10 @@
 data "azurerm_key_vault_secret" "administrator_login_secret" {
-  name         = var.administrator-login-password-secret-name
+  name         = var.administrator_login_secret_name
   key_vault_id = var.key_vault_id
 }
 
 data "azurerm_key_vault_secret" "administrator_login_password_secret" {
-  name         = var.administrator-login-password-secret-name
+  name         = var.administrator_login_password_secret_name
   key_vault_id = var.key_vault_id
 }
 
@@ -12,20 +12,10 @@ resource "azurerm_postgresql_server" "server" {
   name                         = var.server_name
   location                     = var.location
   resource_group_name          = var.resource_group_name
-  administrator_login          = data.azurerm_key_vault_secret.administrator-login-secret.value
-  administrator_login_password = data.azurerm_key_vault_secret.administrator-login-password-secret.value
+  administrator_login          = data.azurerm_key_vault_secret.administrator_login_secret.value
+  administrator_login_password = data.azurerm_key_vault_secret.administrator_login_password_secret.value
   sku_name                     = "B_Gen5_1"
   storage_mb                   = 5120
   version                      = "11"
   ssl_enforcement_enabled      = true
 }
-
-resource "azurerm_postgresql_database" "databases" {
-  count               = length(var.databases)
-  name                = var.databases[count.index]
-  resource_group_name = var.resource_group_name
-  server_name         = azurerm_postgresql_server.server.name
-  charset             = "UTF8"
-  collation           = "English_United States.1252"
-}
-
--- modules/postgresql/variables.tf
+++ modules/postgresql/variables.tf
@@ -14,11 +14,11 @@ variable "key_vault_id" {
   description = "ID of the Azure Key Vault"
 }
 
-variable "administrator-login-secret-name" {
+variable "administrator_login_secret_name" {
   description = "Name of the secret in Azure Key Vault for PostgreSQL server administrator login"
 }
 
-variable "administrator-login-password-secret-name" {
+variable "administrator_login_password_secret_name" {
   description = "Name of the secret in Azure Key Vault for PostgreSQL server administrator password"
 }
 
